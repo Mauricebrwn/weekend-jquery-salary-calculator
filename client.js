792689e54238a2a3ae886590c0263e16898f294e
Maurice Brown <mauricebrwn6890@gmail.com>
2022-12-02 19:13:49 -0600

    added delete button for table row

---
+++ client.js
@@ -0,0 +1,14 @@
+$(document).ready(onReady);
+
+let newEmployee = [
+    {
+        firstName: 'Maurice',
+        lastName: 'Brown',
+        id: 1308,
+        title: 'General Manager',
+        annualSalary: 65000
+    }
+]
+function onReady() {
+    
+}
